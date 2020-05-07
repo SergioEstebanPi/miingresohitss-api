870423d80a5ab8e033be72b4eec1ab6396f1567a
unknown <sepi_147@hotmail.com>
2020-05-06 23:17:26 -0500

    Agrego proyecto commit inicial

---
+++ src/main/java/com/globalhitss/miingresohitss/MiingresohitssApplication.java
@@ -0,0 +1,13 @@
+package com.globalhitss.miingresohitss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiingresohitssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiingresohitssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globalhitss/miingresohitss/MiingresohitssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalhitss.miingresohitss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiingresohitssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
